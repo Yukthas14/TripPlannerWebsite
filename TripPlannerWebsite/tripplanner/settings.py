498ef3dffe8d1d8e6d3c9ea2454767309eb01ade
Yuktha <yukthaappi14@gmail.com>
2026-05-20 21:15:49 +0530

    Fix allowed hosts

--- TripPlannerWebsite/tripplanner/settings.py
+++ TripPlannerWebsite/tripplanner/settings.py
@@ -10,7 +10,11 @@ SECRET_KEY = "django-insecure-change-this-secret-key-before-deployment"
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "tripplannerwebsite-yvkh.onrender.com",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
